a3050577a1828458b76def0e575eef912e9c4afe
Mudit <muditgupta1711@gmail.com>
2026-02-24 10:43:43 +0530

    Implement application launch and teardown methods; add configuration properties

--- src/main/java/com/flipkart/BaseClass/Library.java
+++ src/main/java/com/flipkart/BaseClass/Library.java
@@ -1,5 +1,44 @@
 package com.flipkart.BaseClass;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class Library {
+
+	public static WebDriver driver;
+	public static Properties prop;
+
+	public void launchApplication() throws IOException {
+		FileInputStream input = new FileInputStream(
+				"/Flipkart_Automation/src/test/resources/Properties/Config.Property");
+		prop = new Properties();
+		prop.load(input);
+
+		try {
+			if (prop.getProperty("browser").equalsIgnoreCase("chrome")) {
+				WebDriverManager.chromedriver().setup();
+				ChromeOptions options = new ChromeOptions();
+				options.addArguments("--incognito");
+				driver = new ChromeDriver(options);
+			}
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			driver.get(prop.getProperty("url"));
+		} catch (Exception e) {
+			System.out.println("Browser not launched");
+		}
+	}
+
+	public void teardown() {
+		driver.close();
+	}
 
 }
